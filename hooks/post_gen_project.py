fb698ac65ae6290be7f2e7ec8f5b0da85f4d4c43
Martin Lang <martin.lang@mpsd.mpg.de>
2025-06-16 16:44:18 +0200

    Add post_gen hooks

---
+++ hooks/post_gen_project.py
@@ -0,0 +1,27 @@
+# Shell commands via python to be platform-independent
+import subprocess as sp
+
+
+def init_git_repo():
+    sp.check_call(["git", "init", "."])
+
+
+def add_git_remote():
+    sp.check_call(
+        [
+            "git",
+            "remote",
+            "add",
+            "origin",
+            "git@github.com:MaMMoS-project/{{ cookiecutter.project_name }}.git"
+        ]
+    )
+
+def install_pre_commit_hooks():
+    sp.check_call(["pre-commit", "install"])
+
+
+if __name__ == "__main__":
+    init_git_repo()
+    add_git_remote()
+    install_pre_commit_hooks()
